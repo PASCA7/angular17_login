8b34283a0e4fdc775d511af9e694a34aeeed5f43
PASCA7 <9890015099y@gmail.com>
2024-03-16 16:31:01 +0530

    Check

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+//import { FormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
