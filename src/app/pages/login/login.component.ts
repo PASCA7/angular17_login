7f0dac032d17f19d501ba2ccf672d288f7515e8e
PASCA7 <9890015099y@gmail.com>
2024-03-16 15:38:51 +0530

    16032024

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, HttpClientModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginObj : Login
+
+  constructor(private http:HttpClient){
+    this.loginObj =  new Login();
+  }
+
+  onLogin(){
+    this.http.post('https://miniprojectideas.com/api/User/Login', this.loginObj).subscribe((res:any)=>{
+      if(res.result){
+        alert(true);
+      }else {
+        alert(false)
+      }
+    });
+  }
+}
+
+export class Login {
+
+  EmailId: string;
+  Password: string;
+
+  constructor() {
+    this.Password="";
+    this.EmailId="";
+  }
+}
